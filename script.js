48fd54f83e7b802fb4c6603d004201dddecf5102
Da yoon <gangd0642@gmail.com>
2024-12-10 16:19:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    "use strict";
+    testFun1();
+});
 
+const h1 = document.querySelector("h1");
+
+function testFun1() {
+    fetch("https://codingapple1.github.io/price.json")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            h1.innerHTML = "price : " + data.price;
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+        .finally(() => {
+            console.log("요청 끝");
+        });
+}
